067c916d661f4d6d2cf8a22e49fad885aea69b58
Anna <annaspi333@gmail.com>
2023-05-04 14:42:04 +0200

    Create class exception

--- city-register/src/main/java/edu/javacourse/city/dao/PersonCheckDao.java
+++ city-register/src/main/java/edu/javacourse/city/dao/PersonCheckDao.java
@@ -1,5 +1,12 @@
 package edu.javacourse.city.dao;
 
+import edu.javacourse.city.domain.PersonRequest;
+import edu.javacourse.city.domain.PersonResponse;
+import edu.javacourse.city.exception.PersonCheckException;
+
 public class PersonCheckDao {
 
+    public PersonResponse checkPerson(PersonRequest request) throws PersonCheckException {
+
+    }
 }
