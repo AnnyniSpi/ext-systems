89154a1c8aec567ad1e0858546d5dfd2b04719d3
Anna <annaspi333@gmail.com>
2023-05-04 14:33:11 +0200

    Update register.sql

--- city-register/src/main/resources/register.sql
+++ city-register/src/main/resources/register.sql
@@ -24,7 +24,7 @@ CREATE TABLE cr_address
     street_code   INTEGER            NOT NULL REFERENCES cr_street (street_code) ON DELETE RESTRICT,
     building      VARCHAR(10)        NOT NULL,
     extension     VARCHAR(10),
-    apartment     VARCHAR(10),
+    apartment     VARCHAR(10)
 );
 
 CREATE TABLE cr_person
@@ -46,7 +46,8 @@ CREATE TABLE cr_address_person (
     address_id INTEGER NOT NULL REFERENCES cr_address (address_id) ON DELETE RESTRICT ,
     person_id INTEGER NOT NULL REFERENCES  cr_person (person_id) ON DELETE RESTRICT ,
     start_date DATE NOT NULL ,
-    end_date DATE
+    end_date DATE,
+    temporal BOOLEAN DEFAULT FALSE
 );
 
 -- INSERT
@@ -75,14 +76,14 @@ INSERT INTO cr_person(sur_name, given_name, patronymic, date_of_birth, passport_
                       passport_number, passport_date, c_certificate_number, c_certificate_date)
 VALUES ('Васильев','Флександр','Павлович','2018-10-24', null, null, null, '321456', '2018-11-09');
 
-INSERT INTO cr_address_person(address_id, person_id, start_date, end_date)
-VALUES (1, 1, '2014-10-12', null);
+INSERT INTO cr_address_person(address_id, person_id, start_date, end_date, temporal)
+VALUES (1, 1, '2014-10-12', null, false);
 
-INSERT INTO cr_address_person(address_id, person_id, start_date, end_date)
-VALUES (1, 2, '2014-10-12', null);
+INSERT INTO cr_address_person(address_id, person_id, start_date, end_date, temporal)
+VALUES (1, 2, '2014-10-12', null, false);
 
-INSERT INTO cr_address_person(address_id, person_id, start_date, end_date)
-VALUES (1, 3, '2016-02-05', null);
+INSERT INTO cr_address_person(address_id, person_id, start_date, end_date, temporal)
+VALUES (1, 3, '2016-02-05', null, false);
 
-INSERT INTO cr_address_person(address_id, person_id, start_date, end_date)
-VALUES (1, 4, '2018-12-11', null);
+INSERT INTO cr_address_person(address_id, person_id, start_date, end_date, temporal)
+VALUES (1, 4, '2018-12-11', null, false);
